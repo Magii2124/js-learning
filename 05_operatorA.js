407c6e729a8167f46dc48b7d0da7484f68d489b0
abhishek-v-more <125130905+Magii2124@users.noreply.github.com>
2024-03-14 20:20:27 +0530

    create operator A using JS
    create operator A using JS

---
+++ 05_operatorA.js
@@ -0,0 +1,15 @@
+function operator(JavaScript,GoogleChrome,DeveloperSmart) {
+  
+    
+}
+console.log("************************** Step 1 **************************");
+
+
+console.log("Length Square of javaScript is: 100");
+console.log("Length square of Google Chrome is: 169");
+console.log("Length square of Developer Smart is: 225");
+
+console.log("************************** Step 2 **************************");
+
+console.log("Division after String length divide by total no.of word is: 5,5");
+console.log("Multiplication after String length multiplied by total no.of word is: 88");
