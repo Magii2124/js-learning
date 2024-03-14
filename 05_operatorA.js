704ed3068be46f0175250eedbcd317298ece2c2d
abhishek-v-more <125130905+Magii2124@users.noreply.github.com>
2024-03-14 20:34:54 +0530

    update operator A using JS
    update operator A using JS

--- 05_operatorA.js
+++ 05_operatorA.js
@@ -5,11 +5,15 @@ function operator(JavaScript,GoogleChrome,DeveloperSmart) {
 console.log("************************** Step 1 **************************");
 
 
-console.log("Length Square of javaScript is: 100");
-console.log("Length square of Google Chrome is: 169");
-console.log("Length square of Developer Smart is: 225");
+console.log('Length Square of Word "javaScript" is: 100');
+console.log('Length square of Word "Google Chrome" is: 169');
+console.log('Length square of Word "Developer Smart" is:  225');
 
 console.log("************************** Step 2 **************************");
 
-console.log("Division after String length divide by total no.of word is: 5,5");
+
+console.log('Length of Give String is: 22');
+console.log('Total Number Available in Given String is: 4');
+
+console.log("Division after String length divide by total no.of word is: 5.5");
 console.log("Multiplication after String length multiplied by total no.of word is: 88");
